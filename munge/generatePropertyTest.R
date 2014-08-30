0b695afc42aade1e2242fd4f0f96e0aaeb00f805
Jim Thompson <jimthompson5802@aol.com>
2014-08-30 16:54:59 -0400

    added code to select area of interest

--- munge/generatePropertyTest.R
+++ munge/generatePropertyTest.R
@@ -2,7 +2,18 @@
 # Generate simulated property data
 ###
 
-library(maps)
+library(maptools)
 
 
-# list virginia counties
+# read census.gov county shapefile data
+us.counties <- readShapeSpatial("./data/tl_2014_us_county/tl_2014_us_county.shp",
+                               proj4string = CRS("+proj=longlat +datum=WGS84"))
+
+# read census.gov state shapefile data
+us.states <-readShapeSpatial("./data/tl_2014_us_state/tl_2014_us_state.shp",
+                            proj4string = CRS("+proj=longlat +datum=WGS84"))
+
+# select only states of interest
+states.of.interest <- subset(us.states,STUSPS %in% c("VA"), select=GEOID)
+counties.of.interest <- subset(us.counties,STATEFP %in% states.of.interest$GEOID)
+
