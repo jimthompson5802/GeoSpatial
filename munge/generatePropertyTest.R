853e592666d47df02c975462fa55a5e763107ee1
Jim Thompson <jimthompson5802@aol.com>
2014-09-01 10:59:35 -0400

    modified to allow execution of R code during document creation.  Added ability to dynamically reference values in the document to calculated R variables.

--- munge/generatePropertyTest.R
+++ munge/generatePropertyTest.R
@@ -6,26 +6,27 @@ library(ggmap)
 library(ggplot2)
 library(maptools)
 library(sp)
+library(raster)
+library(rgeos)
 
-source("./src/CommonFunctions.R")
-
+source("CommonFunctions.R")
 
 # read census.gov county shapefile data
-us.counties <- readShapeSpatial("./data/tl_2014_us_county/tl_2014_us_county.shp",
-                               proj4string = CRS("+proj=longlat +datum=WGS84"))
+us.counties <- readShapeSpatial("../data/tl_2014_us_county/tl_2014_us_county.shp",
+                                proj4string = CRS("+proj=longlat +datum=WGS84"))
 
 # read census.gov state shapefile data
-us.states <-readShapeSpatial("./data/tl_2014_us_state/tl_2014_us_state.shp",
-                            proj4string = CRS("+proj=longlat +datum=WGS84"))
+us.states <-readShapeSpatial("../data/tl_2014_us_state/tl_2014_us_state.shp",
+                             proj4string = CRS("+proj=longlat +datum=WGS84"))
 
 # select only states of interest
 states.of.interest <- subset(us.states,STUSPS %in% c("VA"), select=GEOID)
 counties.of.interest <- subset(us.counties,STATEFP == 51 &
-                               NAME %in% c("Arlington", "Fairfax",
-                                           "Alexandria",
-                                           "Loudoun","Culpeper",
-                                           "Rappahannock", "Fauquier",
-                                           "Stafford","Prince William"))
+                                   NAME %in% c("Arlington", "Fairfax",
+                                               "Alexandria",
+                                               "Loudoun","Culpeper",
+                                               "Rappahannock", "Fauquier",
+                                               "Stafford","Prince William"))
 
 
 ###
@@ -42,7 +43,8 @@ generatePropertyData <- function(sp, num.pts=5) {
     colnames(coords) <- c("long","lat")
     
     # compute bounding box for the region
-    bb <- c(min(coords[,"long"]),min(coords[,"lat"]),max(coords[,"long"]),max(coords[,"lat"]))
+    bb <- c(min(coords[,"long"]),min(coords[,"lat"]),
+            max(coords[,"long"]),max(coords[,"lat"]))
     names(bb) <- c("ll.lon","ll.lat","ur.lon","ur.lat")
     
     # randomly "place" points in the region, this is not perfect some will be out of region
@@ -59,7 +61,7 @@ ll <- lapply(attr(counties.of.interest,"polygons"), generatePropertyData,10)
 df <- data.frame(do.call(rbind,ll))
 
 property.locations <- SpatialPointsDataFrame(df[,1:2],data=data.frame(value=df[,3]),
-                                proj4string=CRS("+proj=longlat +datum=WGS84"))
+                                             proj4string=CRS("+proj=longlat +datum=WGS84"))
 # makes points are in the counties of interest
 sp.polygons <- SpatialPolygons(Srl=attr(counties.of.interest,"polygons"))
 proj4string(sp.polygons) <- CRS(proj4string(counties.of.interest))
@@ -67,25 +69,24 @@ flag <- over(property.locations,sp.polygons)
 df$col <- factor(ifelse(!is.na(flag),"in","out"),levels=c("in","out"))
 df <- df[df$col=="in",]
 
-this.map <- get_map("arlington, virginia",9)
-county.boundaries <- cropToMap(this.map,counties.of.interest)
-
-ggmap(this.map) +
-    geom_point(aes(x=lon, y=lat, color=col), 
-               data=df, 
-               shape=16, size=4) +
-    scale_color_manual(values=c("red","blue"))+
-    geom_polygon(aes(x=long, y=lat, group=id), 
-                 data=county.boundaries,
-                 color="red",alpha=0) +
-    geom_text(aes(x=as.numeric(as.character(INTPTLON)), 
-                  y=as.numeric(as.character(INTPTLAT)), label=NAME),
-              data=attr(counties.of.interest,"data"),
-              size=3) +
-    theme_nothing()
+# this.map <- get_map("arlington, virginia",9)
+# county.boundaries <- cropToMap(this.map,counties.of.interest)
+# 
+# ggmap(this.map) +
+#     geom_point(aes(x=lon, y=lat), 
+#                data=df, 
+#                color="black", shape=16, size=3) +
+#     geom_polygon(aes(x=long, y=lat, group=id), 
+#                  data=county.boundaries,
+#                  color="red",alpha=0) +
+#     geom_text(aes(x=as.numeric(as.character(INTPTLON)), 
+#                   y=as.numeric(as.character(INTPTLAT)), label=NAME),
+#               data=attr(counties.of.interest,"data"),
+#               size=3) +
+#     theme_nothing()
 
 # Save property locations for analysis
 property.df <- subset(df,select=-col)
-save(property.df,file="./data/property_locations.RData")
-
-
+property.count <- nrow(property.df)
+property.value<- sum(floor(property.df$value))
+save(property.df,file="../data/property_locations.RData")
--- src/drawHurricanePath.R
+++ src/drawHurricanePath.R
@@ -1,34 +1,37 @@
 ###
-# example code to draw Hurrican Storm Path
+# example code to draw Hurricane Storm Path
 ###
 
 library(ggmap)
-library(ggplot2)
 library(maptools)
 library(rgeos)
 library(raster)
 
 
-source("./src/CommonFunctions.R")
+source("CommonFunctions.R")
 
 # retrieve simulated property location data
-load("./data/property_locations.RData")
+load("../data/property_locations.RData")
+
+# convert property location to Spatial data for testing in or out of region
+property.locations <- SpatialPoints(property.df[,1:2],
+                                    proj4string=CRS("+proj=longlat +datum=WGS84"))
 
 # read census.gov county shapefile data
-us.counties <- readShapeSpatial("./data/tl_2014_us_county/tl_2014_us_county.shp",
+us.counties <- readShapeSpatial("../data/tl_2014_us_county/tl_2014_us_county.shp",
                                 proj4string = CRS("+proj=longlat +datum=WGS84"))
 
 # read census.gov state shapefile data
-us.states <-readShapeSpatial("./data/tl_2014_us_state/tl_2014_us_state.shp",
+us.states <-readShapeSpatial("../data/tl_2014_us_state/tl_2014_us_state.shp",
                              proj4string = CRS("+proj=longlat +datum=WGS84"))
 
 # select only counties of interest
 counties.of.interest <- subset(us.counties, STATEFP == 51 &
-                               NAME %in% c("Arlington", "Fairfax",
-                                           "Alexandria",
-                                           "Loudoun","Culpeper",
-                                           "Rappahannock", "Fauquier",
-                                           "Stafford","Prince William"))
+                                   NAME %in% c("Arlington", "Fairfax",
+                                               "Alexandria",
+                                               "Loudoun","Culpeper",
+                                               "Rappahannock", "Fauquier",
+                                               "Stafford","Prince William"))
 
 
 # generate mape at requested location and zoom level
@@ -49,13 +52,13 @@ storm.map <- ggmap(base.map) +
               fontface="bold", color="red", size=3) +
     theme_nothing()
 
-png("./figures/base_property_locations.png")
+png("../figures/base_property_locations.png")
 print(storm.map)
 dev.off()
 
 
 # retieve storm path shapefile
-storm.cone <- readShapeSpatial("./nhcdata/al182012_5day_025/al182012.025_5day_pgn.shp",
+storm.cone <- readShapeSpatial("../nhcdata/al182012_5day_025/al182012.025_5day_pgn.shp",
                                proj4string = CRS("+proj=longlat +datum=WGS84"))
 
 
@@ -68,35 +71,37 @@ storm.path.to.display <- cropToMap(ec,storm.path)
 
 labpt <- attr(attr(storm.path,"polygons")[[1]],"labpt")
 
-png("./figures/high-level_storm_path.png")
-ggmap(ec) +
+storm.map <- ggmap(ec) +
     geom_polygon(aes(x=long, y=lat, group=id), 
                  data=storm.path.to.display,
-                 color="red",fill="yellow", alpha=0.2,size=0.3) +
+                 color="red",fill="yellow", alpha=0.2,size=1) +
     geom_text(aes(x=labpt[1], y=labpt[2]), 
               label="Hurricane Sandy\n72-hour Forecast Path", 
               size=10,
               color="red") +
-#     ggtitle("Hurricane Sandy 3-Day Forecast Path as of 10/28/2012") +
     theme_nothing()
+png("../figures/high-level_storm_path.png")
+print(storm.map)
 dev.off()
 
 ec <- get_map("arlington, va",7)
 storm.path.to.display <- cropToMap(ec,storm.path)
+states.to.display <- cropToMap(ec,us.states)
 
-png("./figures/mid-level_storm_path.png")
+png("../figures/mid-level_storm_path.png")
 ggmap(ec) +
+    geom_polygon(aes(x=long, y=lat, group=id),
+                 data=states.to.display,
+                 color="black",alpha=0,size=0.3) +
     geom_polygon(aes(x=long, y=lat, group=id), 
                  data=storm.path.to.display,
                  color="red",fill="yellow", alpha=0.2,size=0.3) +
-#     ggtitle("Hurricane Sandy 3-Day Forecast Path as of 10/28/2012") +
+    #     ggtitle("Hurricane Sandy 3-Day Forecast Path as of 10/28/2012") +
     theme_nothing()
 dev.off()
 
 # determine the properties in the storm path region
-# convert property location to Spatial data for testing in or out of region
-property.locations <- SpatialPoints(property.df[,1:2],
-                                    proj4string=CRS("+proj=longlat +datum=WGS84"))
+
 
 # extract out storm path polygon data for testing
 sp.storm <- SpatialPolygons(Srl=attr(storm.path,"polygons"))
@@ -105,6 +110,7 @@ proj4string(sp.storm) <- CRS(proj4string(storm.path))
 # determine the properties in the storm path region
 flag <- over(property.locations,sp.storm)
 property.df$col <- factor(ifelse(!is.na(flag),"in","out"),levels=c("in","out"))
+property.df$pch <- ifelse(!is.na(flag),"17","16")
 property.count <- length(flag)
 property.value<- sum(floor(property.df$value))
 property.count.at.risk <- sum(!is.na(flag))
@@ -114,9 +120,9 @@ property.value.at.risk <- sum(floor(property.df$value[is.na(flag)]))
 # plot property locations
 # print map with property locations
 storm.map <- ggmap(base.map) +
-    geom_point(aes(x=lon, y=lat, color=col), 
+    geom_point(aes(x=lon, y=lat, color=col, shape=pch), 
                data=property.df, 
-               shape=16, size=3) +
+               size=3) +
     scale_color_manual(values=c("red","black"))+
     geom_polygon(aes(x=long, y=lat, group=id), 
                  data=county.boundaries,
@@ -134,9 +140,9 @@ storm.map <- storm.map +
     geom_polygon(aes(x=long, y=lat, group=id), 
                  data=storm.path.to.display,
                  color="red",fill="yellow", alpha=0.2,size=0.3) +
-#     ggtitle("Hurricane Sandy Affected Areas") +
+    #     ggtitle("Hurricane Sandy Affected Areas") +
     theme_nothing()
 
-png("./figures/affected_properties.png")
+png("../figures/affected_properties.png")
 print(storm.map)
 dev.off()
